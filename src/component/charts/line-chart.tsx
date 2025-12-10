533212934595e358e988eaa0c3d7376ae3809b70
hyeon <nguswjd02@gmail.com>
2025-12-10 21:25:54 +0900

    라인 차트 추가

--- src/component/charts/line-chart.tsx
+++ src/component/charts/line-chart.tsx
@@ -1,5 +1,27 @@
+import React from "react";
+import ReactECharts from "echarts-for-react";
+
 function LineChart() {
-  return <div>라인차트</div>;
+  const option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: "line",
+        smooth: true,
+      },
+    ],
+  };
+
+  return (
+    <ReactECharts option={option} style={{ height: "400px", width: "100%" }} />
+  );
 }
 
 export default LineChart;
